a543968f6eb3dbe367ce6fcb6e78533285e397ff
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-05-30 09:24:29 +0800

    Add files via upload

---
+++ jd_cs.py
@@ -0,0 +1,78 @@
+﻿
+# cron "30 22 * * *" 
+# new Env('京东CK-PY测试')
+import json
+import math
+import random
+import threading
+import time
+import requests,os
+import datetime
+
+
+
+# 读取环境变量
+def get_env(env):
+    try:
+        if env in os.environ:
+            a=os.environ[env]
+        elif '/ql' in os.path.abspath(os.path.dirname(__file__)):
+            try:
+                a=v4_env(env,'/ql/config/config.sh')
+            except:
+                a=eval(env)
+        elif '/jd' in os.path.abspath(os.path.dirname(__file__)):
+            try:
+                a=v4_env(env,'/jd/config/config.sh')
+            except:
+                a=eval(env)
+        else:
+            a=eval(env)
+    except:
+        a=''
+    return a
+
+# v4
+def v4_env(env,paths):
+    b=re.compile(r'(?:export )?'+env+r' ?= ?[\"\'](.*?)[\"\']', re.I)
+    with open(paths, 'r') as f:
+        for line in f.readlines():
+            try:
+                c=b.match(line).group(1)
+                break
+            except:
+                pass
+    return c 
+
+
+
+
+## 获取cooie
+class Judge_env(object):
+    def main_run(self):
+        if '/jd' in os.path.abspath(os.path.dirname(__file__)):
+            cookie_list=self.v4_cookie()
+        else:
+            cookie_list=os.environ["JD_COOKIE"].split('&')       # 获取cookie_list的合集
+        if len(cookie_list)<1:
+            print('请填写环境变量JD_COOKIE\n')    
+        return cookie_list
+
+    def v4_cookie(self):
+        a=[]
+        b=re.compile(r'Cookie'+'.*?=\"(.*?)\"', re.I)
+        with open('/jd/config/config.sh', 'r') as f:
+            for line in f.readlines():
+                try:
+                    regular=b.match(line).group(1)
+                    a.append(regular)
+                except:
+                    pass
+        return a
+cookie_list=Judge_env().main_run()   
+
+mycookies = [cookie_list]
+
+
+
+        print(mycookies)
