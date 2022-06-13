12f595be72b6e91c95cba389df2da5cb2be08ab0
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-06-13 18:59:25 +0800

    Add files via upload

--- jd_sj.py
+++ jd_sj.py
@@ -1,5 +1,5 @@
 ﻿# cron "10 22 * * *" 
-# new Env('py三时间比对')
+# new Env('py平台时间【比对】')
 import json
 import math
 import random
@@ -16,7 +16,7 @@ mycookies=[cookie[0],cookie[1],cookie[2],cookie[3],cookie[4],cookie[5],cookie[6]
 #print(mycookies)
 
 #阿里在线获取时间
-def jdtime1():
+def alitime():
   ntp_server_url="ntp.aliyun.com"
   ntp = ntplib.NTPClient()
   ntpResponse = ntp.request(ntp_server_url)
@@ -25,9 +25,23 @@ def jdtime1():
       ntp_time = int(ntpResponse.tx_time * 1000)
       return(ntp_time)
 
+#拼多多时间
+def pddtime():
+    url = 'http://api.pinduoduo.com/api/server/_stm'
+    headers = {
+        "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+    }
+
+    try:
+        res = requests.get(url=url, headers=headers, timeout=1).json()
+        return int(res['server_time'])
+    except:
+        return 0
+
+
 #京东时间
 
-def jdtime2():
+def jdtime():
     url = 'http://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
     headers = {
         "Cookie": cookie[5],"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
@@ -39,10 +53,25 @@ def jdtime2():
     except:
         return 0
 
-#本地时间
-def jdtime3():
-    return int(time.time() * 1000)
+#获取系统时间：		
+def getBdTime():
+     #nt为现在时间，格式为 年月日时分秒
+     nt = datetime.datetime.now()
+     #ntdx为现在时间的 转换时间戳 的 对象，用于转换时间戳
+     ntdx = nt.timetuple()
+     #ntmc为现在时间的 秒时间戳
+     ntmc = time.mktime(ntdx)
+     #nthc为现在时间-毫秒时间戳  .microsecond属性返回给定Time对象2022-06-11 19:37:17.289437中的微秒值289437
+     nthc = int(ntmc*1000 + nt.microsecond/1000)
+     return(nthc)
+print("平台时间：")
 
-print("阿里在线",jdtime1())
-print("京东时间",jdtime2())
-print("本地时间",jdtime3())
+print("阿里在线",alitime())
+print("拼刀时间",pddtime())
+print("京东时间",jdtime())
+print("本地时间",getBdTime())
+
+c=jdtime()-getBdTime()
+print("本地与京东时差:",c,"毫秒")
+
+
