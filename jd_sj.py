19f4942e752c445f9d23afd65d870416db5a92ce
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-06-18 21:37:04 +0800

    Add files via upload

--- jd_sj.py
+++ jd_sj.py
@@ -1,5 +1,5 @@
 ﻿# cron "10 22 * * *" 
-# new Env('py时间与IP【比对】')
+# new Env('py时间与IP比对')
 import json
 import math
 import random
@@ -12,7 +12,7 @@ import ntplib
 #os.environ 获取环境变量
 cookie =os.environ["JD_COOKIE"].split('&')
 #split()：拆分字符串。通过指定分隔符对字符串进行切片，并返回分割后的字符串列表（list）
-mycookies=[cookie[0],cookie[1],cookie[2],cookie[3],cookie[4],cookie[5],cookie[6],cookie[7],cookie[8],cookie[9],cookie[10],cookie[11]]
+mycookies=[cookie[0],cookie[1],cookie[2],cookie[3]]
 #print(mycookies)
 
 #阿里在线获取时间
@@ -44,7 +44,7 @@ def pddtime():
 def jdtime():
     url = 'http://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
     headers = {
-        "Cookie": cookie[5],"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+        "Cookie": cookie[2],"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
     }
 
     try:
