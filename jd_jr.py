073677fbe97f6c75d4e3557e7148f3ff690bd01e
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-07-02 00:27:42 +0800

    Add files via upload

--- jd_jr.py
+++ jd_jr.py
@@ -28,13 +28,13 @@ atime=0
 tq=1000   # 提前 于 整点的 时间，单位毫秒
 
 
-#黄鸟包抓包4次url 全部复制进去，  任意一个链接的body复制进去（body为没有编码翻译的那个）。
-url1 = 'https://jccapt-sp.jr.jd.com/portal/m/dailycoupon/receiveCoupon?channelCode=ICON2'
+#抓POST包获取URL,抓GET包获取body1，总览-第一行的URL点击进去，就是下发的内容。
+url1 = 'https://jccamkt.jr.jd.com/mkt/m/activity/award/v2/receiveAward'
 
-body1 = '{"awardId":1011,"activityId":"202205306936964075599798272","activityType":29,"channelCode":"ICON2"}'
+body1 = '{"cardId":92,"activityId":"202206296947817178245414912","activityType":29,"channelCode":"ICON2"}'
 
 
-# 一个链接能发三次 每次最多5链接
+#抓POST包的headers
 
 headers = {
         "Accept": "application/json, text/plain, */*",
@@ -42,8 +42,8 @@ headers = {
         "Accept-Language": "zh-CN,zh;q=0.9",
         "Connection": "keep-alive",
         "Content-Type": "application/json;charset=UTF-8",
-        'origin': 'https://jccapt.jr.jd.com',
-        "Referer": "https://jccapt.jr.jd.com/home?channelCode=ICON2&jrcontainer=h5&jrlogin=true",
+        'origin': 'https://jcca.jd.com/',
+        "Referer": "https://jcca.jd.com/",
         "Cookie": ck,
         "User-Agent": "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
     }
